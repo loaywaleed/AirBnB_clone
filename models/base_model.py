d3e4c95cfe38e41bb8706107040db0865f7ec9fa
Loay Waleed <loaywaleed9@gmail.com>
2023-08-08 21:41:23 +0300

    Added instantation

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,17 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all other classes"""
+    def __init__(self, args, kwargs):
+        """Instantiation of BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.item():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, value, "%Y-%m-%dT%H%M%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Return a string representation of the instance"""

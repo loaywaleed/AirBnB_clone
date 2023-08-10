634eb42d68d281f909480a7e608a0bbcfb92cb75
loay <loaywaleed9@gmail.com>
2023-08-10 18:54:41 +0300

    added documentation and fixed styling

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,30 +3,36 @@
 
 import json
 
+
 class FileStorage():
-	__file_path = "file.json"
-	__objects = {}
+    """Class that adds storage functionality"""
+    __file_path = "file.json"
+    __objects = {}
 
-	def all(self):
-		return FileStorage.__objects
+    def all(self):
+        """Function that shows all created objects"""
+        return FileStorage.__objects
 
-	def new(self, obj):
-		new_key = obj.__class__.__name__ + "." + obj.id
-		FileStorage.__objects[new_key] = obj
+    def new(self, obj):
+        """Creates new object"""
+        new_key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[new_key] = obj
 
-	def save(self):
-		d = {}
-		for key, value in self.__objects.item():
-			d[key] = value.to_dict()
-		with open(FileStorage.__file_path, 'w', encoding="utf-8") as my_file:
-			json.dump(d, my_file)
+    def save(self):
+        """Saves objects to file"""
+        d = {}
+        for key, value in self.__objects.item():
+            d[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as my_file:
+            json.dump(d, my_file)
 
-	def reload(self):
-		try:
-			with open(self.__file_path, encoding="utf-8") as my_file:
-				d = json.load(my_file)
-				for key, value in d.items():
-					cls = value["__class__"]
-					FileStorage.__objects[key] = eval(cls)(**value)
-		except FileNotFoundError:
-			pass
+    def reload(self):
+        """Reloads objects from a file"""
+        try:
+            with open(self.__file_path, encoding="utf-8") as my_file:
+                d = json.load(my_file)
+                for key, value in d.items():
+                    cls = value["__class__"]
+                    FileStorage.__objects[key] = eval(cls)(**value)
+        except FileNotFoundError:
+            pass

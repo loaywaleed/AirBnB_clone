1c2570eb0ef8c724e3dd496f797dea77b75e3552
Asser Elshafey <elshafeyasser@yahoo.com>
2023-08-12 20:21:44 +0300

    HOT FIX

--- console.py
+++ console.py
@@ -1,72 +1,70 @@
 #!/usr/bin/python3
 """Console interpreter"""
-
 import cmd
 from models.base_model import BaseModel
 from models import storage
 
+
 class HBNBCommand(cmd.Cmd):
-	"""A class that implements a command interpreter"""
-	prompt = "(hbnb) "
-	#classes = {"BaseModel": BaseModel}
+    """A class that implements a command interpreter"""
+    prompt = "(hbnb) "
 
-	def do_quit(self, arg):
-		"""Quit command to exit the program"""
-		return True
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
 
-	def do_EOF(self, arg):
-		"""EOF command to exit the program"""
-		print()
-		return True
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()
+        return True
 
-	def emptyline(self):
-		"""Do nothing when an empty line is entered"""
-		pass
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
 
-	def do_create(self, arg):
-		"""Creates an instance.
-		"""
-		if arg == "" or arg is None:
-			print("** class name missing **")
-		elif arg not in storage.classes():
-			print("** class doesn't exist **")
-		else:
-			b = storage.classes()[arg]()
-			b.save()
-			print(b.id)
+    def do_create(self, arg):
+        """Creates an instance.
+        """
+        if arg == "" or arg is None:
+            print("** class name missing **")
+        elif arg not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            b = storage.classes()[arg]()
+            b.save()
+            print(b.id)
 
-	def do_show(self, arg):
-		args = arg.split()
-		if arg == "" or arg is None:
-			print("** class name missing **")
-		elif args[0] not in storage.classes():
-			print("** class doesn't exist **")
-		elif len(args) < 2:
-			print("** instance id missing **") 
-		else:
-			key = f"{args[0]}.{args[1]}"
-			if key not in storage.all():
-				print("** no instance found **")
-			else:
-				print(storage.all()[key])
+    def do_show(self, arg):
+        args = arg.split()
+        if arg == "" or arg is None:
+            print("** class name missing **")
+        elif args[0] not in storage.classes():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[key])
 
-	def do_destroy(self, arg):
-		args = arg.split()
-		if arg == "" or arg is None:
-			print("** class name missing **")
-		elif args[0] not in storage.classes():
-			print("** class doesn't exist **")
-		elif len(args) < 2:
-			print("** instance id missing **") 
-		else:
-			key = f"{args[0]}.{args[1]}"
-			if key not in storage.all():
-				print("** no instance found **")
-			else:
-				del(storage.all()[key])
-				storage.save()
-				
+    def do_destroy(self, arg):
+        args = arg.split()
+        if arg == "" or arg is None:
+            print("** class name missing **")
+        elif args[0] not in storage.classes():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                del(storage.all()[key])
+                storage.save()
 
 
 if __name__ == '__main__':
-	HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
